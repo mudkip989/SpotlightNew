bfcf6c4d7878733bfca30a49a273adc91d89510e
mudkip989 <muddcop@gmail.com>
2023-04-02 17:34:56 -0500

    All +++

---
+++ src/main/java/me/splt/devteam/spotlight/spotlightplugin/SpotlightPlugin.java
@@ -0,0 +1,17 @@
+package me.splt.devteam.spotlight.spotlightplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpotlightPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
